338d52169f361164c9af86da5bd806954d8d4bdf
psolov <psolov@mera.ru>
2019-06-18 14:40:21 +0300

    Tests fixed

--- src/test/kotlin/com/testtask/stackexchangesearch/BootApplicationTests.kt
+++ src/test/kotlin/com/testtask/stackexchangesearch/BootApplicationTests.kt
@@ -1,14 +1,12 @@
 package com.testtask.stackexchangesearch
 
-
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class StackexchangesearchApplicationTests {
 
+@SpringBootTest
+class BootApplicationTests {
     @Test
     fun `Spring context loaded`() {
     }
-
 }
--- src/test/kotlin/com/testtask/stackexchangesearch/SearchControllerTests.kt
+++ src/test/kotlin/com/testtask/stackexchangesearch/SearchControllerTests.kt
@@ -1,11 +1,18 @@
 package com.testtask.stackexchangesearch
 
 import com.testtask.stackexchangesearch.controllers.SearchController
+import com.testtask.stackexchangesearch.models.Question
+import com.testtask.stackexchangesearch.services.QuestionService
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
 
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
@@ -18,8 +25,27 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 @WebMvcTest(SearchController::class)
 internal class SearchControllerTests {
 
+    @TestConfiguration
+    class ControllerTestConfig {
+        @Bean
+        fun service() = mockk<QuestionService>()
+    }
+
     @Autowired
     private lateinit var mockMvc: MockMvc
+
+    @Autowired
+    private lateinit var service: QuestionService
+
+    @BeforeEach
+    fun serviceMockInit(){
+        val searchString = "java"
+        val pagesize = 25
+        val expectedResult = ArrayList<Question>()
+        every { service.getQuestions(searchString, pagesize, 1) } returns expectedResult
+        every { service.getQuestions(searchString, pagesize, 2) } returns expectedResult
+    }
+
 
     @Test
     fun `Search return search page`() {
@@ -30,11 +56,11 @@ internal class SearchControllerTests {
 
     @Test
     fun `Search with param return search page`() {
+
         mockMvc.perform(get("/search?searchString=java"))
                 .andExpect(status().isOk)
                 .andExpect(content().contentType("text/html;charset=UTF-8"))
     }
-
 
     @Test
     fun `Search-json return ResultSearch json`() {
